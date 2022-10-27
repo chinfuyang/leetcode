9364de234cb9a981ebd5115196e5e7cda48cdd3c
Fu <asd5242539@gmail.com>
2022-10-27 14:22:28 +0800

    soution - only recursive (TLE)

--- Q70-Climbing_Stairs/solution.cpp
+++ Q70-Climbing_Stairs/solution.cpp
@@ -1,16 +1,14 @@
 class Solution
 {
 public:
-    std::unordered_map<int, int> step = {{1, 1}, {2, 2}};
+
     int climbStairs(int n)
     {
-        if (step.find(n) != step.end())
-        {
-            return step[n];
-        }
+        if (n == 1)
+            return 1;
+        if (n == 2)
+            return 2;
 
-        step[n] = climbStairs(n - 1) + climbStairs(n - 2);
-
-        return step[n];
+        return climbStairs(n - 1) + climbStairs(n - 2);
     }
 };
