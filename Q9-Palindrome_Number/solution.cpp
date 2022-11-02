f6180c750b8f79b056d345e321c02f2f1995b1c9
Fu <asd5242539@gmail.com>
2022-11-03 00:05:17 +0800

    Q9 solution faster version

--- Q9-Palindrome_Number/solution.cpp
+++ Q9-Palindrome_Number/solution.cpp
@@ -1,29 +1,19 @@
-class Solution {
+class Solution
+{
 public:
-    bool isPalindrome(int x) {
-        if (x < 0) return false;
-        
-        std::queue<int> _q;
-        std::stack<int> _s;
-        
-        while(x > 0)
+    bool isPalindrome(int x)
+    {
+        if (x < 0 || (x != 0 && (x % 10) == 0))
+            return false;
+
+        int reverse = 0;
+
+        while (x > reverse)
         {
-            int digit = x % 10;
-            _q.push(digit);
-            _s.push(digit);
+            reverse = reverse * 10 + x % 10;
             x /= 10;
         }
-        
-        while (! _s.empty())
-        {
-            int front = _q.front();
-            int top = _s.top();
-            _q.pop();
-            _s.pop();
-            if (front != top) return false;
-        }
-        
-        
-        return true;
+
+        return x == reverse || x == reverse / 10;
     }
 };
