cb009bc5f6102a215ac3f7e93775b59d4c6d437c
Fu <asd5242539@gmail.com>
2022-11-04 00:38:05 +0800

    Q572 solution

---
+++ Q572-Subtree_of_Another_Tree/solution.cpp
@@ -0,0 +1,28 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSubtree(TreeNode* root, TreeNode* subRoot) {
+        if (root == nullptr) return false;
+        if (isSameTree(root, subRoot)) {
+            return true;
+        }
+        
+        return isSubtree(root->left, subRoot) || isSubtree(root->right, subRoot);
+    }
+
+    bool isSameTree(TreeNode *rootA, TreeNode *rootB) {
+        if (rootA == nullptr || rootB == nullptr) return rootA == rootB;
+        
+        return rootA->val == rootB->val && isSameTree(rootA->left, rootB->left) && isSameTree(rootA->right, rootB->right);
+    }
+};
