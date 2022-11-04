41d9398974cffa8a10a7e0483acd33a07b392ad1
Fu <asd5242539@gmail.com>
2022-11-04 23:40:03 +0800

    Q977 solution O(n)*

--- Q977-Squares_of_a_Sorted_Array/solution.cpp
+++ Q977-Squares_of_a_Sorted_Array/solution.cpp
@@ -1,39 +1,15 @@
 class Solution {
 public:
 	vector<int> sortedSquares(vector<int>& nums) {
-		std::stack<int> _s;
-		std::vector<int> ans;
-		_s.push(nums[0] * nums[0]);
-
-		for (int i = 1; i < nums.size(); i++)
-		{
-			int square = nums[i] * nums[i];
-			int top = _s.top();
-			if (square > top)
-			{
-				ans.push_back(top);
-				_s.pop();
-				while (!_s.empty() && square > _s.top() )
-				{
-					top = _s.top();
-					ans.push_back(top);
-					_s.pop();
-				}
-				_s.push(square);
-			}
-			else
-			{
-				_s.push(square);
-			}
-		}
-
-		while (!_s.empty())
-		{
-			int top = _s.top();
-			ans.push_back(top);
-			_s.pop();
-		}
-
-		return ans;
+        std::vector<int> ret(nums.size());
+        int l = 0, r = nums.size() - 1;
+        
+        for (int i = nums.size() - 1; i >= 0; i--)
+        {
+            if (std::abs(nums[l]) > std::abs(nums[r])) ret[i] = nums[l] * nums[l++];
+            else ret[i] = nums[r] * nums[r--];
+        }
+        
+		return ret;
 	}
 };
